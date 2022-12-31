a93e4875d7cf37b00f28054dad938bb0d741a256
Raulebc <raulberrios8@gmail.com>
2022-12-31 02:30:37 -0300

    Update vite.config.js
    - It binds the HMR (hot module replacement) to the localhost

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,26 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: 'resources/js/app.js',
-            refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
-        }),
-    ],
+  server: {
+    hmr: {
+      host: 'localhost',
+    },
+  },
+  plugins: [
+      laravel({
+          input: 'resources/js/app.js',
+          refresh: true,
+      }),
+      vue({
+          template: {
+              transformAssetUrls: {
+                  base: null,
+                  includeAbsolute: false,
+              },
+          },
+      }),
+  ],
 });
