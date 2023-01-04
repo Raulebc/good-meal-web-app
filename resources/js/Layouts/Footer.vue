aee528c52e5881b06ca0877e0e01478c2625878c
Raulebc <raulberrios8@gmail.com>
2023-01-03 23:16:13 -0300

    Create component views

---
+++ resources/js/Layouts/Footer.vue
@@ -0,0 +1,27 @@
+<script setup>
+import FootLink from '@/Components/FootLink.vue';
+</script>
+
+<template>
+    <div class="fixed bottom-0 w-full bg-white shadow-lg">
+        <div class="flex justify-between ml-8 mr-8 p-1">
+            <div class="flex flex-col items-center w-full px-1">
+				<FootLink :href="route('stores')"
+                    :active="route().current('stores')"
+                    name="home"
+                >Inicio</FootLink>
+            </div>
+            <div class="flex flex-col items-center w-full px-1">
+				<FootLink :href="route('purchase-orders')" 
+                    :active="route().current('purchase-orders')"
+                    name="purchase-orders">Ordenes</FootLink>
+            </div>
+            <div class="flex flex-col items-center w-full px-1">
+				<FootLink :href="route('profile')" 
+                    :active="route().current('profile')"
+                    name="profile"
+                >Perfil</FootLink>
+            </div>
+        </div>
+    </div> 
+</template>
---
+++ resources/js/Pages/Stores/Show.vue
@@ -0,0 +1,16 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head, usePage } from '@inertiajs/inertia-vue3';
+
+
+defineProps(['store']);
+
+</script>
+
+<template>
+    <Head title="Store" />
+
+    <AuthenticatedLayout>
+		{{ store.name }}
+    </AuthenticatedLayout>
+</template>
