2b5232a80cf62edd00a1d299605f08d0ad098a25
Raulebc <raulberrios8@gmail.com>
2023-01-05 20:18:49 -0300

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,22 +1,50 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# Determine if stdout is a terminal...
+if test -t 1; then
+    # Determine if colors are supported...
+    ncolors=$(tput colors)
+
+    if test -n "$ncolors" && test "$ncolors" -ge 8; then
+        BOLD="$(tput bold)"
+        WHITE="$(tput setaf 7)"
+        GREEN="$(tput setaf 2)"
+        BACK_GREEN="$(tput setab 2)"
+        NC="$(tput sgr0)"
+    fi
+fi
 
 # copiamos el archivo .env.example a .env
 [ ! -f ".env" ] && cp .env.example .env
 
 # Instalamos las dependencias
 php artisan sail:install -n --with=selenium
+echo "${GREEN}Instalación de Sail terminada"
+
 # para este proyecto elegimos la opcion [0] mysql
-php artisan key:generate
+./vendor/bin/sail artisan key:generate
+
+# Removemos cualquier contenedor "orphan" para este proyecto
+docker-compose down --remove-orphans
+
 # Levantamos los contenedores
 ./vendor/bin/sail up -d
+echo "${GREEN}Contenedores levantados"
+
 # Creamos la base de datos
 touch ./database/good_meal.sqlite
+echo "${GREEN}Base de datos creada."
+
 # Ejecutamos las migraciones con los seeders
 ./vendor/bin/sail artisan migrate:fresh --seed
+
 # Instalamos las dependencias de node
 ./vendor/bin/sail npm install
+
 # Compilamos los assets de vue
 ./vendor/bin/sail npm run build
 
-echo -e "\e[32mGood Meal esta listo para usar\e[0m"
+
+printf "\n"
+echo "${WHITE}${BOLD}${BACK_GREEN} Terminado Good Meal esta listo para ser usado.${NC}"
 
